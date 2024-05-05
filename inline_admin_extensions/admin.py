26bf844f1287736e8f5543c35a941b030256d39d
navid <navid.shagho003@gmail.com>
2024-05-05 10:03:20 +0330

    add package

---
+++ inline_admin_extensions/admin.py
@@ -0,0 +1,96 @@
+from django.contrib import admin
+from django.contrib.admin.views.main import ChangeList, ORDER_VAR
+from django.core import exceptions
+from django.core.paginator import EmptyPage, InvalidPage, Paginator
+
+
+class InlineChangeList(object):
+    can_show_all = True
+    multi_page = True
+    get_query_string = ChangeList.__dict__['get_query_string']
+
+    def __init__(self, request, page_num, page_param, paginator):
+        self.show_all = 'all' in request.GET
+        self.page_num = page_num
+        self.page_param = page_param
+        self.paginator = paginator
+        self.result_count = paginator.count
+        self.params = request.GET
+
+
+def pagination_formset_factory(formset_class, request, **cls_attrs):
+    class PaginationFormSet(formset_class):
+        def __init__(self, *args, **kwargs):
+            super(PaginationFormSet, self).__init__(*args, **kwargs)
+
+            qs = self.queryset
+            paginator = Paginator(qs, self.per_page)
+            try:
+                page_num = int(request.GET.get(self.page_param, '1'))
+            except ValueError:
+                page_num = 0
+            self.page_num = page_num
+
+            try:
+                page = paginator.page(page_num)
+            except (EmptyPage, InvalidPage):
+                page = paginator.page(1)
+
+            self.cl = InlineChangeList(request, page_num, self.page_param,
+                                       paginator)
+            self.paginator = paginator
+
+            if self.cl.show_all:
+                self._queryset = qs
+            else:
+                self._queryset = page.object_list
+
+    for k, v in cls_attrs.items():
+        setattr(PaginationFormSet, k, v)
+
+    return PaginationFormSet
+
+
+class PaginationInline(admin.TabularInline):
+    template = 'admin3/edit_inline/tabular_paginated.html'
+    per_page = 10
+
+    @property
+    def page_param(self):
+        return self.model.__name__.lower()
+
+    @property
+    def order_param(self, prefix=ORDER_VAR):
+        return '_'.join([prefix, self.model.__name__.lower()])
+
+    def get_formset(self, request, obj=None, **kwargs):
+        formset_class = super(PaginationInline, self).get_formset(
+            request, obj, **kwargs)
+
+        return pagination_formset_factory(
+            formset_class,
+            request,
+            per_page=self.per_page,
+            page_param=self.page_param,
+            order_param=self.order_param,
+        )
+
+    def get_queryset(self, request):
+        queryset = super(PaginationInline, self).get_queryset(request)
+        return queryset
+
+    def get_ordering(self, request):
+        ordering = super(PaginationInline, self).get_ordering(request)
+        if self.order_param not in request.GET:
+            return ordering
+
+        ordering = []
+        order_params = request.GET.getlist(self.order_param)
+        for p in order_params:
+            none, pfx, idx = p.rpartition('-')
+            try:
+                self.model._meta.get_field(idx)
+                ordering.append(p)
+            except exceptions.FieldDoesNotExist:
+                pass
+        return ordering
---
+++ inline_admin_extensions/templatetags/inline_admin_paginator.py
@@ -0,0 +1,122 @@
+from collections import namedtuple
+import copy
+import types
+
+import django
+from django.template import Library
+from django.contrib.admin.templatetags.admin_list import (
+    paginator_number,
+    pagination
+)
+from django.contrib.admin.utils import flatten_fieldsets
+
+register = Library()
+
+
+@register.simple_tag
+def parameterized_paginator_number(cl, i):
+    '''This ridiculous code makes a copy of paginator_number function
+
+    The PAGE_VAR variable is global in paginator number, this function copies
+    paginator_number and replaces the global PAGE_VAR with a page_param that
+    we can set.
+    '''
+    # copy the globals of paginator_number
+    func_globals_copy = copy.copy(paginator_number.__globals__)
+    func_globals_copy['PAGE_VAR'] = cl.page_param
+
+    paginator_number_func_copy = types.FunctionType(
+        code=paginator_number.__code__,
+        # pass our updated globals
+        globals=func_globals_copy,
+        name='parameterized_paginator_number',
+        argdefs=paginator_number.__defaults__,
+        closure=paginator_number.__closure__,
+    )
+    return paginator_number_func_copy(cl, i)
+
+
+@register.inclusion_tag('admin3/edit_inline/tabular_inline_pagination.html')
+def tabular_inline_pagination(cl):
+    return pagination(cl)
+
+
+def get_updated_url(formset, old_param, new_param):
+    query_dict = formset.cl.params.copy()
+    ordering = query_dict.getlist(formset.order_param, [])
+
+    try:
+        index = ordering.index(old_param)
+        if new_param:
+            ordering[index] = new_param
+        else:
+            del ordering[index]
+    except ValueError:
+        ordering.append(new_param)
+
+    query_dict.setlist(formset.order_param, ordering)
+    return query_dict.urlencode()
+
+
+SortingUrl = namedtuple('SortingUrl', ['sort', 'toggle', 'remove'])
+
+
+def get_toggle_remove_urls(formset, ordering):
+    toggle_remove_urls = {}
+    for param in reversed(ordering):
+        none, pfx, field_name = param.rpartition('-')
+        if pfx == '-':
+            toggle_remove_urls[field_name] = SortingUrl(
+                'ascending',
+                get_updated_url(formset, param, field_name),
+                get_updated_url(formset, param, ''),
+            )
+        elif pfx == '':
+            toggle_remove_urls[field_name] = SortingUrl(
+                'descending',
+                get_updated_url(formset, param, ''.join(['-', param])),
+                get_updated_url(formset, param, '')
+            )
+    return toggle_remove_urls
+
+
+SortableField = namedtuple('SortableField', ['name', 'field', 'urls'])
+
+
+@register.inclusion_tag('admin3/edit_inline/tabular_header_row.html')
+def table_header_row(inline_admin_formset):
+    '''Similar to InlineAdminFormSet.fields but also returns field name
+
+    Update the dict returned by InlineAdminFormSet.fields and update it with
+    additional attributes that we need for adding sorting urls.
+    '''
+    formset = inline_admin_formset.formset
+    query_dict = formset.cl.params.copy()
+    ordering = query_dict.getlist(formset.order_param, [])
+    toggle_remove_urls = get_toggle_remove_urls(formset, ordering)
+
+    try:
+        fk_name = formset.fk.name
+    except AttributeError:
+        fk_name = None
+    field_names = [i for i in flatten_fieldsets(inline_admin_formset.fieldsets)
+                   if i != fk_name]
+
+    fields = []
+    for field_name, field in zip(field_names, inline_admin_formset.fields()):
+        try:
+            urls = toggle_remove_urls[field_name]
+        except KeyError:
+            toggle = get_updated_url(formset, None, field_name)
+            urls = SortingUrl('', toggle, '')
+        fields.append(SortableField(field_name, field, urls))
+
+    return {
+        'fields': fields,
+        'formset': formset,
+    }
+
+
+@register.simple_tag
+def django_version():
+    return django.VERSION
